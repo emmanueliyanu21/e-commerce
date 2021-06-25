0f2dde931c602b31833b7a4daddd44b3d547d537
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2021-06-25 15:54:24 +0100

    adding cart functionality

--- src/pages/CartScreen.js
+++ src/pages/CartScreen.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import Header from '../components/Header.js'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../actions/cartActions";
+import Message from "../components/Message";
 
-function CartScreen() {
+function CartScreen({ match, location, history }) {
+    
+    const productId = match.params.id;
+
+    const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+    const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart; 
+
+  console.log(cartItems);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const checkoutHandler = () => {
+    history.push("/login?redirect=shipping");
+  };
+
     return (
         <div className="cart-body">
             <Header />
